f7824c56ffbd05e6e159060bf84af2bdabad052f
KennyAle <kennyviver@gmail.com>
2024-10-03 11:30:59 -0700

    Exercise 4

--- ex04.js
+++ ex04.js
@@ -4,6 +4,11 @@ const _ = require('lodash');
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 
+const sumOfEvens = _.chain(numbers)
+.filter(n => n % 2 === 0)
+.sum()
+.value()
+
 // your code here
 
 console.log(sumOfEvens); // Expected output: 20
